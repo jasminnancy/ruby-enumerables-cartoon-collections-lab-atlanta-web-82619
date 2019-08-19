3ba18bf1d7fc1707653a94beaa53245426501e18
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-19 03:41:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.map do |cheese|
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.map do |cheese, cheese_types|
+
     if cheese == cheese_types
       return cheese
     end
